a95beb7642546f16a3a2fb730f8c2a39de15afe1
Ashish Bharadwaj <ashishbharadwaj@icloud.com>
2020-08-21 01:25:21 +0530

    Saved Note's date indicator in calendar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends React.Component
     constructor(props)
     {
         super(props)
-        this.state = ipcRenderer.sendSync('getAppState', this.getDateKeyString(new Date()));
+        this.state = ipcRenderer.sendSync('getAppState', this.getDateKeyString(new Date())) || this.getDefaultState(new Date());
         this.state = {...this.state, doOpen: false, defaultTab: "noteBook", aboutIsOpen: false }
         this.onDateChange = this.onDateChange.bind(this);
         this.onNoteChange = this.onNoteChange.bind(this);
@@ -24,16 +24,28 @@ export default class App extends React.Component
         this.serilizeAndStoreState = this.serilizeAndStoreState.bind(this);
         this.onUpdateDefaultTab = this.onUpdateDefaultTab.bind(this);
         this.onAboutConfigChange = this.onAboutConfigChange.bind(this);
+        this.hasStateChanged = this.hasStateChanged.bind(this);
+        this.hasNotebookChanged = this.hasNotebookChanged.bind(this);
+        this.hasNotesChanged = this.hasNotesChanged.bind(this);
+        this.hasTodoChanged = this.hasTodoChanged.bind(this);
     }
+    getDefaultState(date){
+        return {
+            date: date,
+            editorState: '',
+            notes:[],
+            todoState: []
+        }
+    }
+
     onDateChange (newDate, tabToOpen)  {
         this.serilizeAndStoreState()
-        let nextstate = ipcRenderer.sendSync('getAppState',this.getDateKeyString(newDate));  
+        let nextstate = ipcRenderer.sendSync('getAppState',this.getDateKeyString(newDate)) || this.getDefaultState(newDate);  
         if(tabToOpen){
             this.setState({...nextstate, defaultTab: tabToOpen});
         }else{
             this.setState(nextstate);
         }
-        
     }
     onEditorChange(newContent, delta, source, editor)
     {
@@ -51,13 +63,100 @@ export default class App extends React.Component
     }
     //persits the app State
     serilizeAndStoreState(){
-        let val = Object.assign({},this.state);
-        delete val.doOpen;
-        delete val.defaultTab;
-        delete val.aboutWinConfig;
-        ipcRenderer.send('storeAppState', this.getDateKeyString(val.date),JSON.stringify(val));
-        return val;
+        if(this.hasStateChanged()){
+            let val = Object.assign({},this.state);
+            delete val.doOpen;
+            delete val.defaultTab;
+            delete val.aboutWinConfig;
+            ipcRenderer.send('storeAppState', this.getDateKeyString(val.date),JSON.stringify(val));
+        }        
     }
+
+    hasStateChanged(){
+        let ret = false;
+        let lastSavedState = ipcRenderer.sendSync('getAppState',this.getDateKeyString(this.state.date));
+        switch(this.state.defaultTab){
+            case 'noteBook':
+                ret = this.hasNotebookChanged(lastSavedState);
+                break;
+            case 'stickyNotes':
+                ret = this.hasNotesChanged(lastSavedState);
+                break;
+            case 'toDo':
+                ret = this.hasTodoChanged(lastSavedState);
+                break;
+            default:
+                ret = false;
+                break;
+        }
+        return ret;
+    }
+    hasNotebookChanged(lastSavedState){
+        let currentEditorState = this.state.editorState.replace(/\s*\<.*?\>\s*/g, "").trim();
+        if(lastSavedState){
+            let lastEditorState = lastSavedState.editorState.replace(/\s*\<.*?\>\s*/g, "").trim();
+            return currentEditorState !== lastEditorState;
+        }
+        else{
+            return currentEditorState ? true : false;
+        }
+    }
+    hasNotesChanged(lastSavedState){
+        let ret = false;
+        if(lastSavedState){
+            if(this.state.notes.length === 1){
+                if(this.state.notes.length === lastSavedState.notes.length){
+                    ret = this.hasNoteChanged(lastSavedState.notes[0], this.state.notes[0]);
+                }else{
+                    ret = true;
+                }
+            }
+            else if(this.state.notes.length > 1){
+                ret =  true;
+            }            
+        }
+        else{
+            if(this.state.notes.length === 1){
+                ret = this.state.notes[0].text.trim() ? true : false;
+            }
+            else if(this.state.notes.length > 1){
+                ret = true;
+            }
+        }
+        return ret;
+    }
+    hasNoteChanged(prevNote, nextNote){
+        let keysToCompare = ['color', 'text', 'hidden', 'width', 'height', 'position']
+        for(let key in nextNote){
+            if(keysToCompare.includes(key)){
+                if(prevNote.hasOwnProperty(key)){
+                    if(key === 'position'){
+                        if(prevNote[key].x !== nextNote[key].x || prevNote[key].y !== nextNote[key].y){
+                            return true;
+                        }
+                    }
+                    else{
+                        if(nextNote[key] !== prevNote[key]){
+                            return true;
+                        }
+                    }                    
+                }
+                else{
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+    hasTodoChanged(lastSavedState){
+        if(lastSavedState){
+            return true;
+        }else{
+            return this.state.todoState.length > 0;
+        }
+    }
+
     getDateKeyString(date)
     {
         return (date.getFullYear() + "-" + (date.getMonth() + 1) + "-" +  date.getDate())
